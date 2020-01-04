b4f02542201b044d2d2eed55c16f82c8c3ad409c
daugihao <wojreynolds@gmail.com>
2020-01-04 21:40:41 +0000

    More tidying of 1

--- tarot_slave_1/tarot_slave_1.ino
+++ tarot_slave_1/tarot_slave_1.ino
@@ -9,27 +9,9 @@
 #define RST_PIN 8
 #define SS_PIN  9
 
-// List of Tags UIDs that are allowed to open the puzzle
-byte tagarray[][4] = {
-  {0x5C, 0xF9, 0x9D, 0xD6},
-  {0xCC, 0x21, 0x9D, 0xD6}, 
-  {0xDC, 0xCB, 0x9C, 0xD6},
-  {0xEC, 0xC5, 0x9C, 0xD6},
-  {0xCC, 0x1D, 0x9D, 0xD6},
-};
-
-// Inlocking status :
-int tagcount = 0;
-bool access = false;
-
-#define NR_OF_READERS   1
-
 // Create an MFRC522 instance :
 MFRC522 mfrc522[1];
 
-/**
-   Initialize.
-*/
 void setup() {
 
   SPI.begin();                  // Init SPI bus
@@ -47,10 +29,6 @@ void setup() {
   //mfrc522[0].PCD_SetAntennaGain(mfrc522[0].RxGain_max);
 }
 
-/*
-   Main loop.
-*/
-
 void loop() {
 
   int reader = 0;
@@ -62,49 +40,6 @@ void loop() {
       dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
       Serial.println();
 
-      for (int x = 0; x < sizeof(tagarray); x++)                  // tagarray's row
-      {
-        for (int i = 0; i < mfrc522[reader].uid.size; i++)        //tagarray's columns
-        {
-          if ( mfrc522[reader].uid.uidByte[i] != tagarray[x][i])  //Comparing the UID in the buffer to the UID in the tag array.
-          {
-            DenyingTag();
-            break;
-          }
-          else
-          {
-            if (i == mfrc522[reader].uid.size - 1)                // Test if we browesed the whole UID.
-            {
-              AllowTag();
-            }
-            else
-            {
-              continue;                                           // We still didn't reach the last cell/column : continue testing!
-            }
-          }
-        }
-        if (access) break;                                        // If the Tag is allowed, quit the test.
-      }
-
-
-      if (access)
-      {
-        if (tagcount == NR_OF_READERS)
-        {
-          CorrectTag();
-        }
-        else
-        {
-          MoreTagsNeeded();
-        }
-      }
-      else
-      {
-        UnknownTag();
-      }
-      /*Serial.print(F("PICC type: "));
-        MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
-        Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));*/
       // Halt PICC
       mfrc522[reader].PICC_HaltA();
       // Stop encryption on PCD
@@ -112,56 +47,11 @@ void loop() {
     } //if (mfrc522[reader].PICC_IsNewC..
 }
 
-/**
-   Helper routine to dump a byte array as hex values to Serial.
-*/
 void dump_byte_array(byte * buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
   }
-}
-
-void printTagcount() {
-  Serial.print("Tag n°");
-  Serial.println(tagcount);
-}
-
-void DenyingTag()
-{
-  tagcount = tagcount;
-  access = false;
-}
-
-void AllowTag()
-{
-  tagcount = tagcount + 1;
-  access = true;
-}
-
-void Initialize()
-{
-  tagcount = 0;
-  access = false;
-}
-
-void CorrectTag()
-{
-  Serial.println("Correct card presented!");
-  Initialize();
-}
-
-void MoreTagsNeeded()
-{
-  printTagcount();
-  Serial.println("System needs more cards");
-  access = false;
-}
-
-void UnknownTag()
-{
-  Serial.println("This Tag isn't allowed!");
-  printTagcount();
 }
 
 void requestEvents()
